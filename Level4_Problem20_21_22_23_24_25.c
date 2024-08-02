5022065a7ac4605acd4bb8e9a3e091c9c308e6a6
Dhruv Joshi <whodhruvjoshi@gmail.com>
2024-08-02 21:13:01 +0530

    Update and rename Level4_Problem20.c to Level4_Problem20_21_22_23_24_25.c

--- Level4_Problem20_21_22_23_24_25.c
+++ Level4_Problem20_21_22_23_24_25.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    int i = 2;
+    int i = 100;
     int k, count, count1 = 0;
+    int sum = 0 ;
 
-    while(i <= 9) {
+    while(i <= 999) {
         count = 0; // Reset count for each i
         for(k = 1; k <= i; k++) {
             if(i % k == 0)
                 count++;
         }
-        if(count == 2) // A prime number has exactly two divisors
+        if(count == 2)
+        {// A prime number has exactly two divisors
             count1++;
+            sum = sum+i;
+        }
         i++;
     }
 
     printf("The total number of single digit prime numbers is: %d\n", count1);
+    printf("The total sum of single digit prime numbers is: %d\n", sum);
     return 0;
 }
