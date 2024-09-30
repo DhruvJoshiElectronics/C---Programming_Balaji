5c7ccebc1d516c2daa52487e8b2f720f535e327b
Dhruv Joshi <whodhruvjoshi@gmail.com>
2024-09-30 23:30:21 +0530

    Update Level4_Problem56.c

--- Level4_Problem56.c
+++ Level4_Problem56.c
@@ -1,19 +1,79 @@
-// Create a link list for the following structure and get user inputs of
-// id, Maths mark, Science Mark. Once the user inputs id as -1 exit the
-// input mode and display all the entries.
-// ********** Hint ***************
-// struct student {
-// int id;
-// int Maths;
-// int Science;
-// struct student *next;
-// }
+/******************************************************************************
+        
+            struct student {
+int id;
+int Maths;
+int Science;
+struct student *next;
+}
+        
+*******************************************************************************/
 
-#include<stdio.h>
+#include <stdio.h>
+#include<stdlib.h>
 
-int main(void)
+int main()
 {
-  
+    struct student 
+    {
+    int id;
+    int Maths;
+    int Science;
+    struct student *next;
+    };
+    struct student *head , *newnode , *temp ;
+    head=0;
+    
+    while(1)
+    {
+        newnode = (struct student*)malloc(sizeof(struct student));  // allocation of memory for new newnode
+        if (newnode == NULL) 
+        {
+            printf("Memory allocation failed\n");
+            return -1;  // Return error if malloc fails
+        }
+        
+        printf("Enter Student ID (-1 to exit): ");
+        scanf("%d", &newnode->id);
+        if (newnode->id == -1) 
+        {
+            free(newnode);  // Free the allocated memory for the unused node
+            break;  // Exit the loop when id is -1
+        }
+        
+        printf("Enter Maths Marks: ");
+        scanf("%d", &newnode->Maths);
+        printf("Enter Science Marks: ");
+        scanf("%d", &newnode->Science);
+        
+        //Till here we have created the first node .
+        // Now we will allot null to first node
+        
+        newnode->next = NULL;
+        
+        if(head==NULL)  // This would be only for first node 
+        {
+            head=newnode;
+            temp = newnode;
+        }
+        else
+        {
+         temp->next=newnode;  
+         temp = newnode;
+        }
+    }
+    
+    // To Display All 
+    temp=head;
+        printf("\n The Data entered is : ");
+        while (temp != NULL) 
+        {
+        printf("ID: %d, Maths: %d, Science: %d\n", temp->id, temp->Maths, temp->Science);
+        temp = temp->next;
+    }
+        
+    return 0;
+}
 
 
 
